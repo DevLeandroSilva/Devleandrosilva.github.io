c4259561cef6b5da971435d1e5cfb4dd1b157616
DevLeandroSilva <77212529+DevLeandroSilva@users.noreply.github.com>
2022-08-18 20:55:20 -0300

    Add files via upload

---
+++ LP2/script.js
@@ -0,0 +1,48 @@
+/*
+function desenhaQuadradoVerde(){
+        var tela = document.querySelector('canvas');
+        var pincel = tela.getContext('2d');
+        
+        pincel.fillStyle='green';
+        pincel.fillRect(0,0,600,400);
+}
+
+
+    function desenhaQuadradoAzul(){
+        var tela = document.querySelector('canvas');
+        var pincel = tela.getContext('2d');
+        pincel.fillStyle='blue';
+        pincel.fillRect(0,0,200,400);
+}
+
+
+function desenhaQuadradoAmarelo(){
+        var tela = document.querySelector('canvas');
+        var pincel = tela.getContext('2d');
+        pincel.fillStyle='yellow';
+        pincel.fillRect(0,0,100,400);
+}
+
+    
+
+            function exibeAlerta(){
+                alert("Clique aqui");
+            
+            }
+    
+
+    desenhaQuadradoVerde();
+    desenhaQuadradoAzul();
+    desenhaQuadradoAmarelo();
+    exibeAlerta();
+    */
+            
+
+            
+
+
+
+
+
+
+
